1ec21a6e5d76a9f113c157fb9d4a0e2732218950
wwwalohacampus <h850415@naver.com>
2024-07-26 12:25:17 +0900

    init

---
+++ start/CLI/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#262626',         
+        secondary: '#38BDF8',  
+      }
+    }
+  },
+  plugins: [],
+}
+
